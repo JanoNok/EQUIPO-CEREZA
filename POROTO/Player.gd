dfee954b3b7f864749e5df33a7aef3563ad28776
JanoNok <janoh.power@gmail.com>
2023-08-25 12:13:45 -0400

    Se sube mecanica de dash v1.0

--- POROTO/Player.gd
+++ POROTO/Player.gd
@@ -27,6 +27,13 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, SPEED*direction_y, ACCELERATION)
 	else:
 		velocity.y = move_toward(velocity.y, 0, ACCELERATION)
+		
+	if Input.is_action_just_pressed("dash"):
+		velocity.x = velocity.x*2
+		velocity.y = velocity.y*2
 	
+
+
+
 
 	move_and_slide()
