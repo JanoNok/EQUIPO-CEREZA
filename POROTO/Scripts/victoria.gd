63815988ae20bea0e2b9a3b7bdba110ce82b6a70
SebaVeraniego <alvarogarridobus@gmail.com>
2023-12-12 22:22:08 -0300

    sonidos y bug pantalla de victoria

--- POROTO/Scripts/victoria.gd
+++ POROTO/Scripts/victoria.gd
@@ -1,9 +1,20 @@
 extends Node2D
 @onready var gif_ganar = $Gif_ganar
+@onready var volver_a_jugar = $"MarginContainer/HBoxContainer/volver a jugar"
+@onready var menu_principal = $"MarginContainer/HBoxContainer/menu principal"
 
-func _on_button_pressed():
+
+
+func _ready():
+	volver_a_jugar.pressed.connect(_volver_a_jugar)
+	menu_principal.pressed.connect(_menu_principal)
+
+
+func _volver_a_jugar():
 	get_tree().change_scene_to_file("res://Scenes/Arena.tscn")
 
 
-func _on_button_2_pressed():
+func _menu_principal():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+	
